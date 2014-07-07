ce2e229b30c873b658063a3e3d53987a5275f844
Nataly Daniela Ortiz Arias <naty.dany.98@gmail.com>
2014-07-07 11:41:48 -0400

    correccion ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,7 +9,7 @@ class Ability
     #     can :manage, :all
     #   else
     #     can :read, :all
-    #   end
+       end
     #
     # The first argument to `can` is the action you are giving the user 
     # permission to do.
